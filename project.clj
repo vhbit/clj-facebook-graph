0719ebd73437d477c794eac427a21e3e09c29b09
Valerii Hiora <valerii.hiora@gmail.com>
2013-07-10 14:53:28 +0300

    Updated dependencies to modern versions

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject clj-facebook-graph "0.4.0"
+(defproject clj-facebook-graph "0.4.1-SNAPSHOT"
   :description "A Clojure client for the Facebook Graph API."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/data.json "0.1.1"]
-                 [ring/ring-core "1.0.1"]
-                 [clj-http "0.2.6"]
-                 [clj-oauth2 "0.1.0"]]
-  :dev-dependencies [[ring/ring-devel "1.0.1"]
-                     [ring/ring-jetty-adapter "1.0.0"]
-                     [compojure "0.6.4"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.json "0.2.2"]
+                 [ring/ring-core "1.2.0"]
+                 [clj-http "0.7.4"]
+                 [clj-oauth2 "0.2.0"]]
+  :dev-dependencies [[ring/ring-devel "1.2.0"]
+                     [ring/ring-jetty-adapter "1.2.0"]
+                     [compojure "1.1.5"]]
   :aot [clj-facebook-graph.FacebookGraphException])
